fec9aeb75610a6cee99cb7cebaa08b44f1c4a437
Daniel C. <154235114+zanncdwbl@users.noreply.github.com>
2025-11-28 12:36:15 +0000

    Update main.lua

--- src/main.lua
+++ src/main.lua
@@ -4,7 +4,7 @@
 local mods = rom.mods
 
 ---@module 'SGG_Modding-ENVY-auto'
-mods['SGG_Modding-ENVY'].auto()
+mods["SGG_Modding-ENVY"].auto()
 
 rom = rom
 
@@ -14,58 +14,68 @@ _PLUGIN = PLUGIN
 game = rom.game
 
 ---@module 'SGG_Modding-ModUtil'
-modutil = mods['SGG_Modding-ModUtil']
+modutil = mods["SGG_Modding-ModUtil"]
 
 ---@module 'SGG_Modding-Chalk'
 chalk = mods["SGG_Modding-Chalk"]
 ---@module 'SGG_Modding-ReLoad'
-reload = mods['SGG_Modding-ReLoad']
+reload = mods["SGG_Modding-ReLoad"]
 
 ---@module 'config'
 config = chalk.auto()
 public.config = config
 
 local function on_ready()
-    if config.enabled == false then return end
+	if config.enabled == false then
+		return
+	end
 
-    Gods = {}
-    GodData = {}
-    ActiveBoons = {}
-    BoonData = {}
-    Save = {}
-    SaveName = "Name"
-    SaveDesc = "My bans"
+	Gods = {}
+	GodData = {}
+	ActiveBoons = {}
+	BoonData = {}
+	Save = {}
+	SaveName = "Name"
+	SaveDesc = "My bans"
 
-    game.OnAnyLoad {function()
-        processBans()
-    end}
+	game.OnAnyLoad({
+		function()
+			processBans()
+		end,
+	})
 end
 
 local function on_reload()
-    import 'func.lua'
-    import 'imgui.lua'
-
-    populateBoons()
+	import("func.lua")
+	import("imgui.lua")
 end
 
 local loader = reload.auto_single()
 
 modutil.once_loaded.game(function()
-    loader.load(on_ready, on_reload)
+	loader.load(on_ready, on_reload)
+end)
+
+mods.on_all_mods_loaded(function()
+	modutil.once_loaded.game(function()
+		populateBoons()
+	end)
 end)
 
 modutil.once_loaded.save(function()
-    if config.enabled == false then return end
+	if config.enabled == false then
+		return
+	end
 
-    processBans()
+	processBans()
 
-    local namespace = _PLUGIN.guid
-    if modutil.mod.Mod.Data[namespace] == nil then
-        modutil.mod.Mod.Data[namespace] = {}
-    end
+	local namespace = _PLUGIN.guid
+	if modutil.mod.Mod.Data[namespace] == nil then
+		modutil.mod.Mod.Data[namespace] = {}
+	end
 
-    Save = modutil.mod.Mod.Data[namespace]
-    
-    -- Validate all saved profiles for compatibility issues
-    validateAllSavedProfiles()
+	Save = modutil.mod.Mod.Data[namespace]
+
+	-- Validate all saved profiles for compatibility issues
+	validateAllSavedProfiles()
 end)
